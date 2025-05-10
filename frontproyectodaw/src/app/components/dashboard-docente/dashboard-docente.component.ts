0cf6ac55bb4dfbfc69a12e41ead812e9787f614e
RicardoPH23011 <ph23011@ues.edu.sv>
2025-05-10 17:03:46 -0600

    Agregando componentes base del dashboard docente

---
+++ frontproyectodaw/src/app/components/dashboard-docente/dashboard-docente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-docente',
+  imports: [],
+  templateUrl: './dashboard-docente.component.html',
+  styleUrl: './dashboard-docente.component.scss'
+})
+export class DashboardDocenteComponent {
+
+}
