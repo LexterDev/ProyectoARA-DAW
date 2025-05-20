f1f95775f9b4b6af234be01d286585dfc61513a7
RicardoPH23011 <ph23011@ues.edu.sv>
2025-05-19 20:52:41 -0600

    Se agrego el boton agregar recurso

--- frontproyectodaw/src/app/pages/dashboard-docente/dashboard-docente.component.ts
+++ frontproyectodaw/src/app/pages/dashboard-docente/dashboard-docente.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
+import { ResourceCreateComponent } from '../../components/resource-create/resource-create.component';
+
 
 @Component({
   selector: 'app-dashboard-docente',
@@ -9,13 +17,48 @@ import { CommonModule } from '@angular/common';
   imports: [
     CommonModule,
     MatTabsModule,
-    MatTableModule
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTableModule,
+    MatDialogModule
   ],
   templateUrl: './dashboard-docente.component.html',
   styleUrls: ['./dashboard-docente.component.scss']
 })
 export class DashboardDocenteComponent {
+  // Tabla final
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource: any[] = []; // tabla vacía
+  dataSource: any[] = [];
+
+  // Tabla de recursos subidos
+  uploadedDisplayedColumns: string[] = ['title', 'type', 'uploadedBy'];
+  uploadedResources: any[] = [];
+
+  // Tabla de recursos pendientes
+  pendingDisplayedColumns: string[] = ['title', 'type', 'submittedBy'];
+  pendingResources: any[] = [];
+
+  // Mostrar el componente ResourceEdit
+  mostrarFormulario: boolean = false;
+
+  // Mostrar el componente ResourceCreate
+  constructor(private dialog: MatDialog) {}
+
+  openResourceDialog(): void {
+    const dialogRef = this.dialog.open(ResourceCreateComponent, {
+      width: '700px',
+      disableClose: true
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log('Recurso creado');
+        // Aquí podrías recargar los datos
+      }
+    });
+  }
+
 }
 
